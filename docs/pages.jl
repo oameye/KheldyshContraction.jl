1f1506c317165432103d946833a5f02e0d608622
Orjan Ameye <orjan.ameye@hotmail.com>
2025-04-29 16:08:53 +0200

    docs: add example with literate

--- docs/pages.jl
+++ docs/pages.jl
@@ -4,4 +4,7 @@ pages = [
     "Symbolic fields" => "typesystem.md",
     "Keldysh Conventions" => "conventions.md",
     "Literature" => "literature.md",
+    "Examples" => [
+        "Elastic two body scattering" => "examples/elastic_two_body.md",
+    ]
 ]
