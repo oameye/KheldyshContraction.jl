d38378fffa77d7f54454f3235a69300468161159
Orjan Ameye <orjan.ameye@hotmail.com>
2025-05-03 10:10:04 +0200

    refactor: move filters to seperate file

--- src/wick_contractions.jl
+++ src/wick_contractions.jl
@@ -1,20 +1,4 @@
 #################################
-#       Consistency checks
-#################################
-
-is_qq_contraction(v::Vector{T}) where {T<:QField} = iszero(sum(Int.(contour.(v))))
-# has_qq_contraction(vv::Vector{Vector{<:QField}}) = any(is_qq_contraction.(vv))
-
-function is_physical_propagator(a::Vector{<:QField})
-    len = length(a) == 2 # propagator has length 2
-    positions = position.(a)
-    # ∨ Can't make a propagator with In and Out coordinate
-    in_out = In() ∈ positions ? !(Out() ∈ positions) : true
-    physical = all(is_physical.(a))
-    return len && in_out && physical
-end
-
-#################################
 #       Contraction
 #################################
 """
@@ -47,7 +31,8 @@ function wick_contraction(L::InteractionLagrangian; order=1, simplify=true)
         error("higher order then two not implemented")
     end
     if simplify
-        keldysh, retarded, advanced = advanced_to_retarded.((keldysh, retarded, advanced))
+        keldysh, retarded, advanced =
+            SymbolicUtils.expand.(advanced_to_retarded.((keldysh, retarded, advanced)))
     end
     return DressedPropagator(keldysh, retarded, advanced)
 end
@@ -74,7 +59,7 @@ The function returns a new expression of propagators of type `SymbolicUtils.Symb
 function wick_contraction(a::QAdd; regularise=true)
     wick_contractions = wick_contraction.(SymbolicUtils.arguments(a); regularise)
     wick_contractions = sum(wick_contractions)
-    return make_real(SymbolicUtils.expand(wick_contractions))
+    return make_real(wick_contractions)
 end
 function wick_contraction(a::QMul; regularise=true)
     @assert is_conserved(a)
@@ -136,19 +121,6 @@ function wick_contraction(args_nc::Vector{<:QField})::Vector{Vector{Vector{QFiel
     return wick_contractions
 end
 
-function contraction_filter(v)
-    # istwo = all(length.(v) .== 2) # only two-point contractions
-    # if !istwo
-    #     return false
-    if !is_conserved(v) # contractions with creation/annihilation
-        return false
-    elseif !is_physical_propagator(v) # propagators are physical
-        return false
-    else # there should be no quantum-quantum contractions
-        return !is_qq_contraction(v)
-    end
-end
-
 function make_propagators(contraction::Vector{Vector{Vector{QField}}})::Vector{Vector{SNuN}}
     propagators = map(contraction) do factor
         to_prop = x -> propagator(x...)
@@ -160,49 +132,3 @@ end
 
 make_mul(v::Vector{SNuN}) = isempty(v) ? 0 : prod(v)
 make_term(vp::Vector{Vector{SNuN}}) = isempty(vp) ? 0 : sum(make_mul, vp)
-
-"""
-Filter the zero loops from the list of contractions
-
-Gᴿ(1,2) Gᴿ(2,1) = 0
-Gᴬ(1,2) Gᴬ(2,1) = 0
-Gᴿ(1,2) Gᴬ(1,2) = 0
-"""
-function has_zero_loop(vs::Vector{Vector{QField}}) # TODO: change to Vector{Vector{QSym}}
-    ps = positions.(vs)
-    sorted_ps = sort.(ps)
-    loops = find_equal_pairs(sorted_ps)
-    for loop in loops
-        T1 = propagator_type(vs[loop[1]]...)
-        T2 = propagator_type(vs[loop[2]]...)
-        if all(is_retarded, (T1, T2)) && is_reversed(ps[loop[1]], ps[loop[2]])
-            return true
-        elseif all(is_advanced, (T1, T2)) && is_reversed(ps[loop[1]], ps[loop[2]])
-            return true
-        elseif isequal(ps[loop[1]], ps[loop[2]]) && retarded_and_advanced_pair(T1, T2)
-            return true
-        end
-    end
-    return false
-end
-
-function is_reversed(p1, p2)
-    return isequal(p1[1], p2[2]) && isequal(p1[2], p2[1])
-end
-function retarded_and_advanced_pair(T1, T2)
-    (is_retarded(T1) && is_advanced(T2)) || (is_advanced(T1) && is_retarded(T2))
-end
-function find_equal_pairs(vec)
-    n = length(vec)
-    pairs = Tuple{Int,Int}[]
-
-    for i in 1:n
-        for j in (i + 1):n
-            if isequal(vec[i], vec[j])
-                push!(pairs, (i, j))
-            end
-        end
-    end
-
-    return pairs
-end
