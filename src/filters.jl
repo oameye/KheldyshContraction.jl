3fcdf0441ca6732e2c57d24680d40c87090ef7a2
Orjan Ameye <orjan.ameye@hotmail.com>
2025-05-03 17:58:49 +0200

    refactor: move filters to seperate file

---
+++ src/filters.jl
@@ -0,0 +1,74 @@
+#################################
+#       Consistency checks
+#################################
+
+is_qq_contraction(v::Vector{T}) where {T<:QField} = iszero(sum(Int.(contour.(v))))
+# has_qq_contraction(vv::Vector{Vector{<:QField}}) = any(is_qq_contraction.(vv))
+
+function is_physical_propagator(a::Vector{<:QField})
+    len = length(a) == 2 # propagator has length 2
+    positions = position.(a)
+    # ∨ Can't make a propagator with In and Out coordinate
+    in_out = In() ∈ positions ? !(Out() ∈ positions) : true
+    physical = all(is_physical.(a))
+    return len && in_out && physical
+end
+
+function contraction_filter(v)
+    # istwo = all(length.(v) .== 2) # only two-point contractions
+    # if !istwo
+    #     return false
+    if !is_conserved(v) # contractions with creation/annihilation
+        return false
+    elseif !is_physical_propagator(v) # propagators are physical
+        return false
+    else # there should be no quantum-quantum contractions
+        return !is_qq_contraction(v)
+    end
+end
+
+"""
+Filter the zero loops from the list of contractions
+
+Gᴿ(1,2) Gᴿ(2,1) = 0
+Gᴬ(1,2) Gᴬ(2,1) = 0
+Gᴿ(1,2) Gᴬ(1,2) = 0
+"""
+function has_zero_loop(vs::Vector{Vector{QField}}) # TODO: change to Vector{Vector{QSym}}
+    ps = positions.(vs)
+    sorted_ps = sort.(ps)
+    loops = find_equal_pairs(sorted_ps)
+    for loop in loops
+        T1 = propagator_type(vs[loop[1]]...)
+        T2 = propagator_type(vs[loop[2]]...)
+        if all(is_retarded, (T1, T2)) && is_reversed(ps[loop[1]], ps[loop[2]])
+            return true
+        elseif all(is_advanced, (T1, T2)) && is_reversed(ps[loop[1]], ps[loop[2]])
+            return true
+        elseif isequal(ps[loop[1]], ps[loop[2]]) && retarded_and_advanced_pair(T1, T2)
+            return true
+        end
+    end
+    return false
+end
+
+function is_reversed(p1, p2)
+    return isequal(p1[1], p2[2]) && isequal(p1[2], p2[1])
+end
+function retarded_and_advanced_pair(T1, T2)
+    (is_retarded(T1) && is_advanced(T2)) || (is_advanced(T1) && is_retarded(T2))
+end
+function find_equal_pairs(vec)
+    n = length(vec)
+    pairs = Tuple{Int,Int}[]
+
+    for i in 1:n
+        for j in (i + 1):n
+            if isequal(vec[i], vec[j])
+                push!(pairs, (i, j))
+            end
+        end
+    end
+
+    return pairs
+end
